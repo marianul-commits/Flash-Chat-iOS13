c24a053fffcc29aeab256e92803882589ec9bd5e
marianul-commits <80788036+marianul-commits@users.noreply.github.com>
2023-06-14 23:05:15 +0300

    Completed Flash Chat Course Project

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
@@ -14,6 +17,21 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                    let alert = UIAlertController(title: "Error", message: "\(e.localizedDescription)", preferredStyle: UIAlertController.Style.alert)
+                    alert.addAction(UIAlertAction(title: "Close", style: UIAlertAction.Style.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    self.performSegue(withIdentifier: K.registerSegue, sender: self)
+                }
+            }
+
+        }
+        
     }
     
 }
---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Marian Nasturica on 11.03.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    
+    let sender: String
+    let body: String
+    
+}
